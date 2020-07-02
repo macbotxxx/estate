c56f142867d434b95fa10e8098345651124a58cb
Michael Assanama <assanamamichael@gmail.com>
2020-07-02 13:59:43 +0100

    Adding Forms
    adding forms from the database to update the company infromation

--- properties/forms.py
+++ properties/forms.py
@@ -1,8 +1,32 @@
 from django.forms import ModelForm
-from .models import UploadProperties
+from .models import UploadProperties, HomePageImages, Companyinformation, Testmonials
 
 class UploadProperty(ModelForm):
       class Meta:
             model = UploadProperties
             fields = '__all__'
             exclude = ['slug']
+
+class HomePageImage(ModelForm):
+      class Meta:
+            model = HomePageImages
+            fields = '__all__'
+            exclude = ['company_image']
+
+class CompanyDetails(ModelForm):
+      class Meta:
+            model = Companyinformation
+            fields = '__all__'
+            exclude = ['company_logo_light','company_logo_dark']
+            
+            
+class CompanyLogo(ModelForm):
+      class Meta:
+            model = Companyinformation
+            fields = ['company_logo_light', 'company_logo_dark']
+      
+
+class CustomerReviews(ModelForm):
+      class Meta:
+            model = Testmonials
+            fields = '__all__'
