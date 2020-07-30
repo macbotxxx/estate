3c4eb53d75172530ac8e7b1855cb9267dd709bf5
Michael Assanama <assanamamichael@gmail.com>
2020-07-30 02:22:58 +0100

    templates

--- estate/settings.py
+++ estate/settings.py
@@ -90,7 +90,7 @@ SITE_ID = 1
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [os.path.join(BASE_DIR, "templates/main")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
