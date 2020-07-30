dfb1cde740d684f6c5eda87c99a8941283940efd
Michael Assanama <assanamamichael@gmail.com>
2020-07-30 13:54:52 +0100

    Update settings.py

--- estate/settings.py
+++ estate/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 
 STATICFILES_DIRS = [
