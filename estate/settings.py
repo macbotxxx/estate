b8db9a99c8c5aedcf806eda452f7b5cf41688f45
Michael Assanama <assanamamichael@gmail.com>
2020-07-30 14:02:56 +0100

    Update settings.py

--- estate/settings.py
+++ estate/settings.py
@@ -168,7 +168,7 @@ STATICFILES_DIRS = [
      os.path.join(BASE_DIR, 'static')
 ]
 VENV_PATH = os.path.dirname(BASE_DIR)
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'  
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img') 
 # MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root') 
 
