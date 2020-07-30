5e6dfacec86e56a47e1646882367382ad4be52ef
Michael Assanama <assanamamichael@gmail.com>
2020-07-30 13:47:04 +0100

    Postgre

--- estate/settings.py
+++ estate/settings.py
@@ -110,13 +110,18 @@ WSGI_APPLICATION = 'estate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'roverestate',
+        'USER': 'postgres',
+        'PASSWORD': 'M08034169322i',
+        'HOST': 'localhost',
+        'POST': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
