486ebf5ce7383a8b5caa7bafab11dc6577ec61ec
Michael Assanama <assanamamichael@gmail.com>
2020-08-31 18:03:43 +0100

    Update settings.py

--- estate/settings.py
+++ estate/settings.py
@@ -124,17 +124,24 @@ WSGI_APPLICATION = 'estate.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'roverestate',
+#         'USER': 'postgres',
+#         'PASSWORD': 'M08034169322i',
+#         'HOST': 'localhost',
+#         'POST': '5432',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'roverestate',
-        'USER': 'postgres',
-        'PASSWORD': 'M08034169322i',
-        'HOST': 'localhost',
-        'POST': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 
 # ON_HEROKU = os.environ.get('ON_HEROKU')
